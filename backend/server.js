7ff871a2f1910a5c76791ac95b4931529ecd6ee6
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-02-21 16:46:41 -0500

    Initialization (Completed Tutorial #1 to Tutorial #3)

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// Entry file of the back-end application, where we'll register the Express app
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+// express app
+const app = express()
+
+// built-in middleware for parsing incoming JSON payloads and making that data available in the req.body
+app.use(express.json())
+
+// middleware: runs first before any request (call the next function to call the next request)
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// When we fire a request at req (/api/workouts), we use the routes at res (workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
