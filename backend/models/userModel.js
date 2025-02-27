2aa81dc46015f9f5986b5f0b67f009c27bad4bf7
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-02-27 17:55:20 -0500

    Completed Tutorial #4

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,18 @@ const userSchema = new Schema({
 
 // Satic sign up method
 userSchema.statics.signup = async function(email, password){ // Must be a regular function instead of an arrow function to use "this" keyword
+
+    // Validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
     const exists = await this.findOne({email})
 
     if (exists) {
